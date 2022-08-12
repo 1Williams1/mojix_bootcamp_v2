33d01862ef4ca3b5c3a1de12a816bea41ab67ea7
1Williams1 <williamschavez79@gmail.com>
2022-08-11 20:37:41 -0400

    Creating my first commit for my app1

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+import Image
+
+myImage = Image.open("https://miro.medium.com/max/700/1*5IFgojJ4nU8f0YKTcjWDrg.jpeg");
+myImage.show();
+
 st.write("1. Walrus operator")
 
 st.write("mylist = [1,2,3]")
