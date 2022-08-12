c74715a62a999f4f562733f253d8a5521f955fe9
1Williams1 <williamschavez79@gmail.com>
2022-08-11 20:31:57 -0400

    Creating my first commit for my app1

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 st.write("1. Walrus operator")
 
-mylist = [1,2,3]
-if(l := len(mylist) > 2):
-    print(l)
-
 st.write("mylist = [1,2,3]")
 st.write("if(l := len(mylist) > 2):")
 st.write("print(l)")
 st.write("Output")
-st.write("3")
+mylist = [1,2,3]
+if(l := len(mylist) > 2):
+    st.write(l)
+
+
