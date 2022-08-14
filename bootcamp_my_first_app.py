5221ece9aabb0dd3606183a37da7d4c9ee32c6ae
1Williams1 <williamschavez79@gmail.com>
2022-08-13 22:33:11 -0400

    Creating my first commit for my app

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -34,3 +34,11 @@ st.write("if(l := len(mylist) > 2):")
 st.write("print(l)")
 st.write("Output")
 st.write(3)
+
+code = '''mylist = [1,2,3]
+            if(l := len(mylist) > 2):
+                print(l)
+
+            Output
+            3 '''
+st.code(code, language='python')
